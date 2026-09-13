cd3baf479fec59c63f5165661a553a0ecef2c696
Terracotta Lighting <terracottalighting@gmail.com>
2026-09-12 20:57:48 -0500

    Swap shipment freight field positions

--- src/components/shipping/shipment-freight-fields.tsx
+++ src/components/shipping/shipment-freight-fields.tsx
@@ -14,8 +14,8 @@ export function ShipmentFreightFields({ actualCost = 0, customerCharge, masterTr
 
   return <div className="shipment-freight-fields">
     <label>Master Tracking No.<input defaultValue={masterTrackingNumber} name="master_tracking_number" /></label>
-    <label>Customer Freight Charge<input disabled={freeFreight} min="0" name="shipping_fee" onChange={(event) => setCharge(event.target.value)} step="0.01" type="number" value={freeFreight ? "0.00" : charge} /></label>
     <label>Actual Freight Cost (Internal)<input defaultValue={actualCost} min="0" name="freight_cost" step="0.01" type="number" /></label>
+    <label>Customer Freight Charge<input disabled={freeFreight} min="0" name="shipping_fee" onChange={(event) => setCharge(event.target.value)} step="0.01" type="number" value={freeFreight ? "0.00" : charge} /></label>
     <label className="checkbox-label"><input checked={freeFreight} name="is_free_freight" onChange={(event) => setFreeFreight(event.target.checked)} type="checkbox" /> Free Freight for customer</label>
   </div>;
 }
